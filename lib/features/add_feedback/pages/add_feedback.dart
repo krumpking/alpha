0f2a1d401d9bc780b8165a0551a031cd5a5a39f3
KumaloWilson <kumalowilson900@gmail.com>
2024-09-06 13:08:13 +0200

    added feedback screen

---
+++ lib/features/add_feedback/pages/add_feedback.dart
@@ -0,0 +1,161 @@
+import 'package:alpha/features/add_feedback/helpers/helpers.dart';
+import 'package:alpha/global/global.dart';
+import 'package:alpha/models/user_profile.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import '../../../core/constants/color_constants.dart';
+import '../../../custom_widgets/custom_button/general_button.dart';
+import '../../../custom_widgets/custom_switch/custom_switch.dart';
+import '../../../custom_widgets/text_fields/custom_text_field.dart';
+
+class AddFeedbackScreen extends StatefulWidget {
+  final UserProfile  selectedUser;
+  final String shiftId;
+
+  const AddFeedbackScreen({super.key, required this.selectedUser, required this.shiftId});
+
+  @override
+  State<AddFeedbackScreen> createState() => _AddFeedbackScreenState();
+}
+
+class _AddFeedbackScreenState extends State<AddFeedbackScreen> {
+  final TextEditingController descriptionController = TextEditingController();
+  final TextEditingController feedbackTitleController = TextEditingController();
+  final currentUser = FirebaseAuth.instance.currentUser;
+  FeedbackTag? feedbackTag;
+  bool _isAlpha = false;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Pallete.primaryColor,
+        centerTitle: true,
+        iconTheme: const IconThemeData(color: Colors.white),
+        title: Text(
+          widget.selectedUser.name,
+          style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+
+            Container(
+              width: 150,
+              height: 150,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Pallete.primaryColor,
+                  width: 2,
+                ),
+              ),
+              child: Icon(
+                Icons.feedback,
+                size: 100,
+                color: Pallete.primaryColor,
+              ),
+            ),
+
+            Text(
+              'Add Feedback',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Pallete.primaryColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16
+              ),
+            ),
+
+            const SizedBox(
+              height: 24,
+            ),
+
+            CustomTextField(
+              keyBoardType: TextInputType.text,
+              controller: feedbackTitleController,
+              labelText: 'Title',
+              prefixIcon: const Icon(Icons.description, color: Colors.grey),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            CustomTextField(
+              controller: descriptionController,
+              labelText: 'Description',
+              prefixIcon: const Icon(Icons.description, color: Colors.grey),
+            ),
+
+            const SizedBox(
+              height: 12,
+            ),
+
+            Container(
+              padding: const EdgeInsets.symmetric(
+                  horizontal: 12
+              ),
+              decoration: BoxDecoration(
+                  border: Border.all(
+                      color: Pallete.greyAccent
+                  ),
+                  borderRadius: BorderRadius.circular(10)
+              ),
+              child: ListTile(
+                contentPadding: EdgeInsets.zero,
+                title: const Text(
+                  'Feedback Tag',
+                  style: TextStyle(
+                    fontSize: 12,
+                  ),
+                ),
+                trailing: CustomSwitch(
+                  height: 25,
+                  activeColor: Pallete.primaryColor,
+                  value: _isAlpha,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _isAlpha = value;
+                    });
+                  },
+                ),
+              ),
+            ),
+
+
+            const SizedBox(
+              height: 20,
+            ),
+
+            Center(
+              child: GeneralButton(
+                onTap: ()=> FeedbackHelper.validateAndSubmitShift(
+                  feedbackTag: feedbackTag!,
+                  description: descriptionController.text.trim(),
+                  currentUser: currentUser!,
+                  feedbackTitle: feedbackTitleController.text.trim(),
+                  selectedUser: widget.selectedUser,
+                  shiftId: widget.shiftId,
+                ),
+                borderRadius: 10,
+                btnColor: Pallete.primaryColor,
+                width: 300,
+                child: const Text(
+                  "Add Feedback",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
